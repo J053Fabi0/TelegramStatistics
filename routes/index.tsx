fc89785e9c7d6333c59f2fde7245cc79f8286024
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-27 18:01:03 -0600

    Added instructions and information in the home page

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div>
       <Typography variant="h1">Telegram chat statistics</Typography>
+      <Typography variant="lead">All data is processed locally, not sent to any server.</Typography>
+      <Typography variant="lead">
+        Load the <code>result.json</code> file you get when exporting your chat as JSON with Telegram Desktop.
+      </Typography>
 
       <Statistics />
     </div>
