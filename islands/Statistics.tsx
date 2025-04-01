9d2c3d03ae1076941505673d158aff8ee2b4a77b
Jose Fabio <J053Fabi0@users.noreply.github.com>
2025-04-01 16:09:47 -0600

    Do not validate JSON

--- islands/Statistics.tsx
+++ islands/Statistics.tsx
@@ -1,23 +1,13 @@
 import Charts from "./Charts.tsx";
 import loadJSON from "../utils/loadJSON.ts";
 import { useSignal } from "@preact/signals";
-import validateJSON from "../utils/validateJSON.ts";
 import { TelegramExport } from "../types/telegramExport.type.ts";
 
 export default function Statistics() {
   const data = useSignal<TelegramExport | null>(null);
 
   async function handleFileInput(file: File) {
-    const unknownJson = await loadJSON<TelegramExport>(file);
-
-    const { error, value } = validateJSON(unknownJson);
-    if (error) {
-      console.dir(unknownJson.messages[+(error.details[0].context?.key ?? 0)]);
-      console.dir(error.details[0].context);
-      console.dir(error);
-
-      alert("The file you uploaded is not a valid Telegram export. Check the console for more information.");
-    } else data.value = value;
+    data.value = await loadJSON<TelegramExport>(file);
   }
 
   return (
